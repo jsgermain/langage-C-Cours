080d43a13a264231ddbfce381ac976c1c7c62920
john <john-88@live.fr>
2019-10-28 10:35:26 +0100

    Décrémenter avec l'instruction (for)

--- instructionFor.c
+++ instructionFor.c
@@ -2,14 +2,8 @@
 // un programme permettant de rentrer des entiers et afficher leur somme
 int main()
 {
-    int entier, somme=0;
+    int entier;
 
-    for(int i=1; i<4;i++)
-    {
-        printf("Entrer une valeur \n");
-        scanf("%d", &entier);
-        somme=somme+entier;
-    }
-
-    printf("La somme des element entre est %d", somme);
+    for(entier=10; entier>=5; entier--)
+    printf(" %d \n", entier);
 }
