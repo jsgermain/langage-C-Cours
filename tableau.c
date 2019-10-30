84797ef798bdcffaa7f5f1de28de515c6d051915
john <john-88@live.fr>
2019-10-30 19:27:20 +0100

    Déclaration d'un tableau en (float) et affichage de ses éléments

--- tableau.c
+++ tableau.c
@@ -3,13 +3,13 @@
 int main()
 {
     // Déclaration d'un tableau
+    float notes[4]={5.5,50.2,30.6,80.9};
 
-    int sac[4]={5,50,30,80};
-
+// affichage des éléments du tableau
     printf("Les elements du tableau sont :\n");
 
-    printf("En position[0] = %d \n", sac[0]);
-    printf("En position[1] = %d \n", sac[1]);
-    printf("En position[2] = %d \n", sac[2]);
-    printf("En position[3] = %d \n", sac[3]);
+    printf("En position[0] = %.1f \n", notes[0]);
+    printf("En position[1] = %.1f \n", notes[1]);
+    printf("En position[2] = %.1f \n", notes[2]);
+    printf("En position[3] = %.1f \n", notes[3]);
 }
