1f522c49aa967b1749a1fa1aa26c5e23f240bc90
john <john-88@live.fr>
2019-10-30 16:50:13 +0100

    La différence entre l'instruction for et while

--- instructionWhile.c
+++ instructionWhile.c
@@ -2,12 +2,26 @@
 
 int main()
 {
-    int compteur=1;
+    int compteur=1; 
+
+    /* Dans la boucle while l'initialisation du compteur se fait avant la boucle
+    l'incrémentation est une instruction dans la boucle
+    */
 
     while(compteur<=5)
     {
         printf("compteur = %d \n", compteur);  
         compteur++;
+    }
+
+puts("Ici commence la boucle for");
+
+    /* le compteur est initialisé dans la boucle
+    l'incrémentation se fait dans la boucle
+    */
+    for(compteur=1; compteur<=5; compteur++)
+    {  
+        printf("compteur = %d \n", compteur);
     }
     
     
