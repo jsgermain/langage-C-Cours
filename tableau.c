99007751eb7e1582ef58a38d8c832748f6e4e6ba
john <john-88@live.fr>
2019-10-30 19:31:01 +0100

    Déclaration d'un tableau en (char) et affichage de ses éléments

--- tableau.c
+++ tableau.c
@@ -3,13 +3,13 @@
 int main()
 {
     // Déclaration d'un tableau
-    float notes[4]={5.5,50.2,30.6,80.9};
+    char notes[4]={'j','o','h','n'};
 
 // affichage des éléments du tableau
     printf("Les elements du tableau sont :\n");
 
-    printf("En position[0] = %.1f \n", notes[0]);
-    printf("En position[1] = %.1f \n", notes[1]);
-    printf("En position[2] = %.1f \n", notes[2]);
-    printf("En position[3] = %.1f \n", notes[3]);
+    printf("En position[0] = %c \n", notes[0]);
+    printf("En position[1] = %c \n", notes[1]);
+    printf("En position[2] = %c \n", notes[2]);
+    printf("En position[3] = %c \n", notes[3]);
 }
