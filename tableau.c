b6647656aa353b12ab1034bb8e32cb1755b9594b
john <john-88@live.fr>
2019-10-30 19:23:25 +0100

    Déclaration d'un tableau en (entier) et affichage de ses éléments

---
+++ tableau.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    // Déclaration d'un tableau
+
+    int sac[4]={5,50,30,80};
+
+    printf("Les elements du tableau sont :\n");
+
+    printf("En position[0] = %d \n", sac[0]);
+    printf("En position[1] = %d \n", sac[1]);
+    printf("En position[2] = %d \n", sac[2]);
+    printf("En position[3] = %d \n", sac[3]);
+}
