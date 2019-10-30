3fb7386a48ae44f9dc78ee4696ffa1e257f290e7
john <john-88@live.fr>
2019-10-30 16:35:36 +0100

    Script en vue d'une instruction while

---
+++ instructionWhile.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int compteur=1;
+
+    printf("compteur = %d \n", compteur); 
+    
+    compteur++;
+    printf("compteur = %d \n", compteur); 
+    
+    compteur++;
+    printf("compteur = %d \n", compteur); 
+    
+    compteur++;
+    printf("compteur = %d \n", compteur); 
+    
+    compteur++;
+    printf("compteur = %d \n", compteur); 
+
+
+}
