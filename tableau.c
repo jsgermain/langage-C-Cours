0ea0a49ca55454e3cd72b7a9fe0124a0dbc7dddf
john <john-88@live.fr>
2019-10-30 23:19:51 +0100

    remplir un tableau trouver la valeur maximale du tableau et l'afficher

--- tableau.c
+++ tableau.c
@@ -1,13 +1,26 @@
 #include<stdio.h>
-
+/*
+remplir un tableau trouver la valeur maximale du tableau et l'afficher
+*/
 int main()
 {
-    int region[6]={77,75,95,91,92,93};
-    
-    printf("Les departements de l'IDF sont :\n");
-    
-    for(int i=5; i>=0; i--)
+    // on déclare les variables
+    int t[6]; //le tableau
+    int i; // l'indice
+    int max; // contiendra la valeur max du tableau
+
+// la 1ère boucle permet de remplir le tableau 
+    for(i=0; i<6; i++)
     {
-        printf("En position %d, on trouve le departement du %d \n", i, region[i]);
+        printf("t[%d] => ", i); //t[%d] permet d'afficher le tableau et l'indice, i prend la valeur
+        scanf("%d", &t[i]);
     }
+// la 2ème boucle parcours le tableau pour trouver la valeur max
+    for(max=t[0], i=1; i<6; i++)
+    {
+        if(max < t[i])
+            max = t[i];
+    }
+
+    printf(" La valeur maximum du tableau est : %d", max);
 }
