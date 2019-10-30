ed92bd21b1755ac2b440f299db6ca6d277076199
john <john-88@live.fr>
2019-10-31 00:12:39 +0100

    Tableau  chaine et affichage

---
+++ chaine.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+Comprendre les chaines des caractères
+*/
+int main()
+{
+    char chaine[30];
+    printf("Entrer une chaine :\n");
+    scanf("%s",chaine); // le caractère de formatage d'une chaine (%s) string
+    
+    printf("La chaine = %s", chaine);
+}
