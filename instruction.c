5c02ca093a8ce24373e8675ae1537b7083e4f80e
john <john-88@live.fr>
2019-10-31 13:24:31 +0100

    Table d'addition

--- instruction.c
+++ instruction.c
@@ -4,5 +4,5 @@ int main()
 {
     int compteur=1; // initialiser
     for(; compteur<=10; compteur++) // ; du fait de l'initialisation
-    printf("7x %d = %d \n", compteur, compteur*7);
+    printf("7 + %d = %d \n", compteur, compteur+7);
 }
