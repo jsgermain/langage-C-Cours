88da94eaafc3cf1d9f6f2fb8a47624cdecfe5c1b
john <john-88@live.fr>
2019-10-31 11:06:22 +0100

    Ajouter N caractère à une chaine

--- chaine.c
+++ chaine.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 /*
-Ajouter le contenu d'une chaine à une autre
-strcat(chaine_destination, chaine_source);
+Ajouter N caractère à une chaine
+strncat(chaine_destination, chaine_source, nombre_caractère);
 */
 int main()
 {
     char parent1[30] =" Johnes Saint Germain ";
-    char parent2[30] = " Norane Jean ";
+    char parent2[30] = ", Norane Jean ";
 
     puts(parent1); // parent1 garde sa valeur d'initialisation
-    strcat(parent1,parent2); // ici parent2 s'ajoute au parent1
+    strncat(parent1,parent2, 8); // ici 8 CARACTERE parent2 s'ajoute au parent1
     puts(parent1) ; // ici s'affiche les parent1 et 2
 }
