6b6ecd8a76e72ed7db368633eda9f8467fc76133
john <john-88@live.fr>
2019-10-31 11:00:40 +0100

    Ajouter le contenu d'une chaine à une autre

--- chaine.c
+++ chaine.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 /*
-Copier les carctères d'une chaine vers une autre
-strcpy(chaine_destination, chaine_source);
+Ajouter le contenu d'une chaine à une autre
+strcat(chaine_destination, chaine_source);
 */
 int main()
 {
-    char parent1[30] ="Johnes Saint Germain";
-    char parent2[30] = "Norane Jean";
+    char parent1[30] =" Johnes Saint Germain ";
+    char parent2[30] = " Norane Jean ";
 
     puts(parent1); // parent1 garde sa valeur d'initialisation
-    strcpy(parent1, parent2); // ici parent1 prend la valeur du parent2
-    puts(parent1); // ici parent1 a la valeur de celui de 1
+    strcat(parent1,parent2); // ici parent2 s'ajoute au parent1
+    puts(parent1) ; // ici s'affiche les parent1 et 2
 }
