373850345d8f9aee549a286c4d25b11b7d8440cf
john <john-88@live.fr>
2019-10-31 10:08:13 +0100

    Copier les caractères d'une chaine vers une autre

--- chaine.c
+++ chaine.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 /*
-Le nombre d'élément d'une chaine de caractère 
+Copier les carctères d'une chaine vers une autre
+strcpy(chaine_destination, chaine_source);
 */
 int main()
 {
-    char chaine[30] ="Johnes Saint Germain";
-    printf("Le nombre d'element de la chaine est %d", strlen(chaine));
+    char parent1[30] ="Johnes Saint Germain";
+    char parent2[30] = "Norane Jean";
+
+    puts(parent1); // parent1 garde sa valeur d'initialisation
+    strcpy(parent1, parent2); // ici parent1 prend la valeur du parent2
+    puts(parent1); // ici parent1 a la valeur de celui de 1
 }
